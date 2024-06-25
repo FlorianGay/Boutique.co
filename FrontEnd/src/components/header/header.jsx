a4881b68694514c95831ef531876587c9bf3db93
Florian <floriangay750@gmail.com>
2024-06-25 11:03:52 +0200

    Début création fichier du projet Début création header

---
+++ FrontEnd/src/components/header/header.jsx
@@ -0,0 +1,35 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Logo from '../../assets/boutiqueCo_logo.png'
+import './header.scss'
+
+function Header() {
+  return (
+    <header>
+      <div className="logo_title">
+        <img src={Logo} alt="logo of the website" />
+        <h2>Boutique.Co</h2>
+      </div>
+
+      <nav>
+        <div className="nav_list">
+          <ul>
+            <li>Accueil</li>
+            <li>A propos</li>
+            <li>Catégories</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="nav_option">
+            <ul>
+                <li>Connectez-vous <FontAwesomeIcon icon={faUser} /></li>
+                <li>Panier <FontAwesomeIcon icon={faCartShopping} /></li>
+            </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ FrontEnd/src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+    return (
+        <>
+        <h1>Ceci est la homepage</h1>
+        </>
+    )
+}
+
+export default Home
